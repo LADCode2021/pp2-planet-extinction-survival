77adfa8c932464a97a878df133d3ad86635b6477
LADCode2021 <luke.dawson@live.co.uk>
2022-01-05 16:26:25 +0000

    Add checkAnswer function and DOMonload function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-/**
- * This function loads exinction AI Survival Test on page load.
- * This was copied and adapted from Love Maths walkthrough project
+/**Let's the DOM load before loading function.
+ * Calls button to check answers.
+ * This is taken from Love Math project.
  */
-document.addEventListener("DOMContentLoaded", function () {
+
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function () {
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 checkAnswer();
             } else {
@@ -16,57 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    document.getElementById("answer-box").addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-            checkAnswer();
-        }
-    })
-
-    runGame("addition");
-
 });
-
-
-/**
- * Generates a question randomly from an array of questions.
- * Questions adapted from: https://readymadepubquiz.com/tag/maths/, https://triviaquiznight.com/maths-quiz-questions-and-answers/
- */
-
- function shuffle(array) {
-    // Loop from the last element to the second element
-    for(let i = array.length - 1; i > 0; i--) {
-      // Generate random index from remaining unshuffled elements
-      const j = Math.floor(Math.random() * (i + 1));
-      // Swap elements
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  };
-
-    let questions = shuffle([
-        'What does 6 to the power 0 equal?', //0
-        'What is the highest common factor of 30 and 132?', //1
-        'How many months are there in twelve Earth years?', //2
-        'Geoff thinks of a number. He deducts five from it and then divides the result by three. His answer is 25. What number did he start with?', //3
-        'How many seconds are there in 12 minutes?', //4
-        'Which is the closest prime number to 100?', //5
-        'What is 20% of 1630?', //6
-        'How many minutes are there in a quarter of a day?', //7
-        'What is 80% of 295?', //8
-        'How many millimetres are there in 80cm?' //9
-    ]);
-
-    
-    let currentQuestion = 0;
-    function questionGenerator() {
-      document.getElementById('question-gen').innerHTML = questions[currentQuestion++];
-    }
-
-    questionGenerator();
-
-/**
- * Array of corresponding answers to the questions in questionGenerator()
- */
-let answers = [];
 
 
 /**
@@ -87,7 +38,23 @@ function checkAnswer() {
         800,
     ];
 
+    let qOne = parseInt(document.getElementById('a1').value);
+    let qTwo = parseInt(document.getElementById('a2').value);
+    let qThree = parseInt(document.getElementById('a3').value);
+    let qFour = parseInt(document.getElementById('a4').value);
+    let qFive = parseInt(document.getElementById('a5').value);
+    let qSix = parseInt(document.getElementById('a6').value);
+    let qSeven = parseInt(document.getElementById('a7').value);
+    let qEight = parseInt(document.getElementById('a8').value);
+    let qNine = parseInt(document.getElementById('a9').value);
+    let qTen = parseInt(document.getElementById('a10').value);
 
+    if (qOne === answers[0]) {
+        document.getElementById('a1').style.backgroundColor = 'green';
+    } else {
+        alert(`${qOne} is incorrect. You are one step closer to death`)
+        document.getElementById('a1').style.backgroundColor = 'red';
+    }
 
 }
 
@@ -96,6 +63,8 @@ function checkAnswer() {
  */
 function correctAnswerIncrement() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
 /**
